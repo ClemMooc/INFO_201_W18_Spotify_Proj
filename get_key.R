ccdf5b35cd1bfe1a8cbbd16a7aa88274ceb05bcb
Kris W <kristofer.wong.27@gmail.com>
2018-03-04 16:41:30 -0800

    Keys commit

--- get_key.R
+++ get_key.R
@@ -1,15 +1,18 @@
 library('httr')
-library('dplyr')
 library('jsonlite')
+
 source('Final_Spotify/data/keys.R')
+
 id_secret  <- RCurl::base64(paste(spotify.client_id, spotify.secret, sep = ":"))
+
 my_headers<-add_headers(c(Authorization=paste('Basic',id_secret,sep=' ')))
+
 my_body<-list(grant_type='client_credentials')
+
 spotify <- POST('https://accounts.spotify.com/api/token',
                 my_headers,
                 body = my_body,
                 encode = 'form')
 token <- content(spotify)
-
-spotify.token <- token$access_token
 print(token$access_token)
+spotify.token <- token$access_token

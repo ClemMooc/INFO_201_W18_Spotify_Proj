4f61d60e8b13ec611449db0784fe5b0a3a45a33f
pennert-1630865 <pennert@uw.edu>
2018-03-06 18:18:50 -0800

    added comments

--- Data_Wrangling.R
+++ Data_Wrangling.R
@@ -196,5 +196,19 @@ View(song_info)
              #custom labels
              vlcex=0.8
   )
+
+  ## TO BE PUT IN UI ##
+  ## Energy Variable Description
+  #Energy is a measure from 0 to 100 and represents a perceptual measure of intensity and activity. Typically, energetic tracks 
+  #feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual 
+  #features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.
+  
+  ##Danceability Variable Description
+  #Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm 
+  #stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.
+  
+  ##Valence Variable Description
+  #A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive 
+  #(e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).
   
   
